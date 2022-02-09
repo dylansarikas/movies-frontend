63ef11f09713469819093458429dca9fde5ab0e8
dylan <dylan.sarikas@gmail.com>
2022-02-08 19:40:51 -0600

    Creates movie frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "This is the movies frontend! Not all movies have directors!",
+      movies: [],
+      showDirector: false,
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
+    createMovie: function () {
+      var params = {
+        title: this.title,
+        year: this.year,
+        plot: this.plot,
+        director: this.director,
+      };
+      axios
+        .post("http://localhost:3000/movies", params)
+        .then((response) => {
+          console.log("Here", response.data);
+          this.movies.push(response.data);
+          this.title = "";
+          this.year = "";
+          this.plot = "";
+          this.director = "";
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h1>Create Movie</h1>
+    <p>
+      Title:
+      <input type="text" v-model="title" />
+    </p>
+    <p>
+      Year:
+      <input type="text" v-model="year" />
+    </p>
+    <p>
+      Plot:
+      <input type="text" v-model="plot" />
+    </p>
+    <p>
+      Director:
+      <input type="text" v-model="director" />
+    </p>
+    <button v-on:click="createMovie()">Create</button>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>Movie: {{ movie.title }}</h2>
+      <h4>Year: {{ movie.year }}</h4>
+      <h3>Plot: {{ movie.plot }}</h3>
+      <button v-on:click="showDirector = !showDirector">Show/Hide Director</button>
+      <p v-if="showDirector">{{ movie.director }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
